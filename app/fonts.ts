a2dfab7b733570461b7210d9153ff0d835e97d59
UdemeKen <udemekendrick@gmail.com>
2024-10-19 21:45:55 +0100

    new update

---
+++ app/fonts.ts
@@ -0,0 +1,6 @@
+import { Kanit } from "next/font/google";
+
+export const font = Kanit({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,225 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: {
+				"25": "#FCFAFF",
+				"50": "#F9F5FF",
+				"100": "#F4EBFF",
+				"200": "#E9D7FE", 
+				"300": "#D6BBFB",
+				"400": "#B692F6",
+				"500": "#9E77ED",
+				"600": "#7F56D9",
+				"700": "#6941C6",
+				"800": "#53389E", 
+				"900": "#42307D",
+				"950": "#2C1C5F",
+			}, 
+			secondary: {
+				"25": "#FAFAFF",
+				"50": "#F4F3FF",
+				"100": "#EBE9FE",
+				"200": "#D9D6FE", 
+				"300": "#BDB4FE",
+				"400": "#9B8AFB",
+				"500": "#7A5AF8",
+				"600": "#6938EF",
+				"700": "#5925DC",
+				"800": "#4A1FB8", 
+				"900": "#3E1C96",
+				"950": "#27115F",
+			},
+
+			// Base Color
+			white: "#FFFFFF",
+			black: "#000000",
+			transparent: "#FFFFFF",
+	
+			// Base Color Themes
+			heading: "#1f1f1f",
+			paragraphe: "4b5563",
+			label: "#1f1f1f",
+			placeholder: "#9ca3af",
+			table: colors.gray[900],
+			backgroundDisable: "#F3F4F6",
+	
+			border: "#E5E7EB",
+			"input-border": colors.gray[300],
+			"icon": colors.gray[500],
+  		},
+
+		// Typography
+		fontSize: {
+			sm: "0.8rem",
+			base: "1rem",
+			xl: "1.25rem",
+			"2xl": "1.563rem",
+			"3xl": "1.953rem",
+			"4xl": "2.441rem",
+			"5xl": "3.052rem",
+
+			// custom font size
+			h1: [
+				"4rem", 
+				{
+					lineHeight: "5rem",
+					letterSpacing: "0.0375rem",
+					fontWeight: "700",
+				},
+			],
+			h2: [
+				"3rem", 
+				{
+					lineHeight: "2rem",
+					letterSpacing: "0.035rem",
+					fontWeight: "700",
+				},
+			],
+			h3: [
+				"2.5rem", 
+				{
+					lineHeight: "3.5rem",
+					letterSpacing: "0.05rem",
+					fontWeight: "700",
+				},
+			],
+			h4: [
+				"2rem", 
+				{
+					lineHeight: "2.75rem",
+					letterSpacing: "0rem",
+					fontWeight: "700",
+				},
+			],
+			h5: [
+				"1.5rem", 
+				{
+					lineHeight: "2.5rem",
+					letterSpacing: "0rem",
+					fontWeight: "600",
+				},
+			],
+			h6: [
+				"1.25rem", 
+				{
+					lineHeight: "2rem",
+					letterSpacing: "0rem",
+					fontWeight: "500",
+				},
+			],
+			p: [
+				"0.875rem", 
+				{
+					lineHeight: "1.5rem",
+					letterSpacing: "0rem",
+					fontWeight: "400",
+				},
+			],
+			body: [
+				"1rem", 
+				{
+					lineHeight: "1.5rem",
+					letterSpacing: "0rem",
+					fontWeight: "400",
+				},
+			],
+			span: [
+				"0.75rem", 
+				{
+					lineHeight: "1.5rem",
+					letterSpacing: "0rem",
+					fontWeight: "400",
+				},
+			],
+		},
+
+		// Screens
+		screens: {
+			'xs': '360px',
+			// => @media (min-width: 360px) { ... }
+
+			'sm': '575px',
+			// => @media (min-width: 575px) { ... }
+
+			'md': '768px',
+			//  => @media (min-width: 768px) { ... }
+
+			'lg': '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			'xl': '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1480px',
+			// => @media (min-width: 1480px) { ... }
+
+			'3xl': '1680px',
+			// => @media (min-width: 1680px) { ... }
+
+			'4xl': '1920px',
+			// => @media (min-width: 1920px) { ... }
+		},
+
+		container: {
+			center: true,
+			screens: {
+				'xs': '360px',
+				// => @media (max-width: 360px) { ... }
+
+				'sm': '640px',
+				// => @media (max-width: 640px) { ... }
+
+				'md': '768px',
+				//  => @media (max-width: 768px) { ... }
+	
+				'lg': '1024px',
+				// => @media (max-width: 1024px) { ... }
+	
+				'xl': '1280px',
+				// => @media (max-width: 1280px) { ... }
+	
+				'2xl': '1480px',
+				// => @media (max-width: 1480px) { ... }
+	
+				'3xl': '1680px',
+				// => @media (max-width: 1680px) { ... }
+
+				'4xl': '1920px',
+				// => @media (max-width: 1920px) { ... }
+			},
+			padding: {
+				DEFAULT: "0rem",
+				xs: "0rem",
+				sm: "0rem",
+				md: "0rem",
+				lg: "0rem",
+				xl: "0rem",
+				"2xl": "2rem",
+				"3xl": "2rem",
+				"4xl": "2rem",
+			},
+		},
+
+		fontWeight: {},
+
+
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
